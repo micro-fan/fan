65ae3c8798e524bf47a8bd621e354fe94ec001f3
Kirill Pinchuk <cybergrind@gmail.com>
2016-08-26 00:20:34 +0300

    Add AMQPTransport

---
+++ fan/contrib/amqp/__init__.py
@@ -0,0 +1,115 @@
+import asyncio
+import asynqp
+import re
+from types import CoroutineType
+
+from basictracer.context import SpanContext
+
+from fan.context import TracedContext as Context
+from fan.remote import RemoteEndpoint
+from fan.contrib.aio.remote import AIOTransport, AIOQueueBasedTransport
+
+
+asynqp.queue.VALID_QUEUE_NAME_RE = re.compile('')
+
+class AMQPTransport(AIOQueueBasedTransport, AIOTransport):
+
+    async def ensure_connection(self):
+        if hasattr(self, 'conn'):
+            return
+        params = self.params
+        self.conn = await asynqp.connect(params.get('host', 'localhost'),
+                                         params.get('port', 5672),
+                                         username=params.get('user', 'guest'),
+                                         password=params.get('password', 'guest'))
+
+    async def sub_prepare(self):
+        params = self.params
+        await self.ensure_connection()
+        self.sub = await self.conn.open_channel()
+        self.routing_key = params['routing_key']
+        self.default_exchange = await self.sub.declare_exchange('', 'direct')
+        self.exchange = await self.sub.declare_exchange(params.get('exchange', ''),
+                                                        params.get('exchange_type', 'direct'))
+        print('Subscribe...')
+        if self.remote:
+            queue_name = params['queue']
+            queue = await self.sub.declare_queue(queue_name)
+            await queue.bind(self.exchange, self.routing_key)
+        else:
+            self.responses = asyncio.Queue()
+            queue_name = 'amq.rabbitmq.reply-to'
+            queue = await self.sub.declare_queue(queue_name, nowait=True)
+        # self.loop.create_task(self.read_loop(queue))
+        self.queue = queue
+        await queue.consume(self.deliver, no_ack=not self.remote)
+
+
+    async def pub_prepare(self):
+        self.pub = await self.conn.open_channel()
+
+    async def sub_stop(self):
+        await self.sub.close()
+        await self.conn.close()
+
+    async def pub_stop(self):
+        await self.pub.close()
+
+    async def on_start(self):
+        print('Start amqp')
+        await super().on_start()
+
+    async def rpc_inner_call(self, msg):
+        ctx = msg['context_headers']
+        for k, v in ctx.items():
+            ctx[k] = str(v)
+        body = msg
+        amqp_msg = asynqp.Message(body,
+                                  headers=ctx,
+                                  # expiration=str(time.time()+10),
+                                  reply_to='amq.rabbitmq.reply-to',
+                                  correlation_id=str(ctx['span_id']))
+        print('Publish message: {} {}'.format(self.exchange, self.routing_key))
+        self.exchange.publish(amqp_msg, self.routing_key)
+        rep = await self.responses.get()
+        return rep['response']
+
+    def deliver(self, msg):
+        print('DELIVERED: {}'.format(msg))
+        self.loop.create_task(self.read_loop(msg))
+
+    async def read_loop(self, raw_msg):
+        print('Got message: {} Q[{}]'.format(raw_msg, self.queue.name))
+
+        msg = raw_msg.json()
+        ctx_headers = msg['context_headers']
+        parent_ctx = SpanContext(**ctx_headers)
+        method = msg['method']
+        ctx = Context(self.discovery, parent_ctx, method)
+        print('CTX: {} {}'.format(ctx.span.context.trace_id, raw_msg.correlation_id))
+        args = msg.get('args', ())
+        kwargs = msg.get('kwargs', {})
+        if self.remote:
+            hc = self.handle_call(method, ctx, *args, **kwargs)
+            if isinstance(hc, CoroutineType):
+                resp = await hc
+            else:
+                resp = hc
+            print('Send resp ==> : {}'.format(resp))
+            resp = asynqp.Message({'context_headers': msg['context_headers'],
+                                   'method': msg['method'],
+                                   'response': resp},
+                                  correlation_id=raw_msg.correlation_id)
+            self.default_exchange.publish(resp, raw_msg.reply_to, mandatory=False)
+            raw_msg.ack()
+        else:
+            print('Put into responses {}'.format(self.responses))
+            await self.responses.put(msg)
+
+
+class AMQPEndpoint(RemoteEndpoint):
+    async def on_start(self):
+        await self.transport.on_start()
+
+    async def on_stop(self):
+        await self.transport.on_stop()
---
+++ fan/contrib/amqp/tests/test_amqp.py
@@ -0,0 +1,91 @@
+import asyncio
+import os
+from unittest import TestCase
+
+from basictracer import BasicTracer
+from basictracer.recorder import InMemoryRecorder
+
+from fan.context import TracedContext
+from fan.remote import ProxyEndpoint
+from fan.contrib.amqp import AMQPEndpoint, AMQPTransport
+from fan.discovery import SimpleDictDiscovery, LocalDiscovery
+from fan.contrib.aio.discovery import AIOCompositeDiscovery
+from fan.service import Service, endpoint
+
+
+class DummyDiscovery(SimpleDictDiscovery):
+    pass
+
+
+class TestLocalDiscovery(LocalDiscovery):
+    transports = {'amqp': AMQPTransport}
+
+    def get_transport_class(self, name):
+        return self.transports[name]
+
+
+class DummyService(Service):
+    service_name = 'dummy'
+
+    @endpoint
+    def ping(self, ctx):
+        print('Call ping endpoint {}'.format(ctx.span.context.trace_id))
+        return 'pong'
+
+
+class RedisCase(TestCase):
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+        self.recorder = InMemoryRecorder()
+        self.dict_discovery = DummyDiscovery({})
+        self.discovery = AIOCompositeDiscovery(TestLocalDiscovery(), self.dict_discovery)
+        self.discovery.tracer = BasicTracer(self.recorder)
+        self.svc = DummyService()
+        self.svc.on_start()
+
+    def test_remote_register(self):
+        ep = AMQPEndpoint(self.discovery, self.svc, {})
+        ep.on_start()
+        self.discovery.register(ep)
+
+        l = self.discovery.local
+        r = self.discovery.remote
+        assert l.cached_endpoints[('dummy',)] == ep
+        assert r.data['dummy'] == {}
+
+    @property
+    def ctx(self):
+        discovery = AIOCompositeDiscovery(TestLocalDiscovery(), self.dict_discovery)
+        discovery.tracer = BasicTracer(self.recorder)
+        return TracedContext(discovery)
+
+    async def _test_remote_call(self):
+        params = {'host': os.environ.get('AMQP_HOST', 'rabbitmq'),
+                  'queue': 'dummy',
+                  'exchange': 'tipsi',
+                  'exchange_type': 'topic',
+                  'routing_key': 'dummy',
+                  'transport': 'amqp'}
+        ep = AMQPEndpoint(self.discovery, self.svc, params)
+        print('EP: {} {}'.format(ep.on_start(), ep))
+        await ep.on_start()
+        self.discovery.register(ep)
+
+        pep = ProxyEndpoint(self.discovery, 'dummy', params)
+        pep.on_start()
+
+        ctx = self.ctx
+        res = await ctx.rpc.dummy.ping()
+        ctx.span.finish()
+        self.assertEqual(res, 'pong')
+
+    def test_remote_call(self):
+        self.loop.run_until_complete(self._test_remote_call())
+        self.assertEquals(len(self.recorder.get_spans()), 2)
+        print('SPANS: {}'.format([(x.context.span_id, x.context.trace_id) for x in self.recorder.get_spans()]))
+
+    def tearDown(self):
+        self.loop.close()
+        asyncio.set_event_loop(None)
