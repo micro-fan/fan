91bbc3fa126dbaf5a1b435ff46f0b2b6a7833842
Kirill Pinchuk <cybergrind@gmail.com>
2016-09-09 02:23:23 +0300

    ZKDiscovery: store dummy endpoint

--- fan/contrib/aio/discovery.py
+++ fan/contrib/aio/discovery.py
@@ -1,6 +1,4 @@
-import asyncio
-
-from aiozk import ZKClient
+from aiozk import ZKClient, exc
 
 from fan.discovery import CompositeDiscovery, RemoteDiscovery
 from fan.contrib.aio.remote import AIOProxyEndpoint
@@ -11,13 +9,34 @@ class ZKDiscovery(RemoteDiscovery):
         super().__init__()
         self.zk = ZKClient(zk_path, chroot)
 
+    async def create(self, path, **kwargs):
+        try:
+            return await self.zk.create(path, **kwargs)
+        except exc.NodeExists:
+            pass
+
+    async def recursive_create(self, path, version, data):
+        if not isinstance(path, list):
+            path = path.split('/')
+        curr = '/endpoints'
+        await self.create(curr)
+        for sub in path:
+            curr += '/{}'.format(sub)
+            await self.create(curr, container=True)
+        curr += '/{}'.format(version)
+        await self.create(curr, container=True)
+        curr += '/config_'
+        await self.create(curr, data=data, ephemeral=True, sequential=True)
+
     async def on_start(self):
         await self.zk.start()
         if not await self.zk.exists('/endpoints'):
             await self.zk.create('/endpoints')
 
-    def register(self, endpoint):
+    async def register(self, endpoint):
         name = endpoint.name
+        path = name.split('.')
+        await self.recursive_create(path, endpoint.version, endpoint.config)
 
     def find_endpoint(self, service_name):
         pass
--- fan/contrib/aio/tests/test_zk.py
+++ fan/contrib/aio/tests/test_zk.py
@@ -1,23 +1,35 @@
 import asyncio
-
 import os
-from unittest import TestCase
+
+# TODO separate
+from aiozk.test.aio_test import AIOTestCase
 
 from fan.contrib.aio.discovery import ZKDiscovery
 
 
-class TestZK(TestCase):
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
+class StubEndpoint:
+    name = 'this.is.stub.endpoint'
+    version = '1.0.0'
+    config = '{"stub": "config"}'
 
+
+class TestZK(AIOTestCase):
+    async def setUp(self):
         self.remote = ZKDiscovery(os.environ.get('ZK_HOST', 'zk:2181'), chroot='/test_fan')
-
-    async def _init_inner(self):
         await self.remote.on_start()
-        self.ok = True
 
-    def test_init(self):
-        self.loop.run_until_complete(self._init_inner())
-        assert self.ok
+    async def test_init(self):
         pass
+
+    async def test_register_function(self):
+        data = b'{"test": "data"}'
+        await self.remote.recursive_create('/test/sub1/sub2', '1.0.0', data)
+        base_path = '/endpoints/test/sub1/sub2/1.0.0'
+        names = await self.remote.zk.get_children(base_path)
+        out = await self.remote.zk.get_data('{}/{}'.format(base_path, names[0]))
+        self.assertEqual(data, out)
+        await self.remote.zk.deleteall('/endpoints/test')
+
+    async def test_register_endpoint(self):
+        ep = StubEndpoint()
+        await self.remote.register(ep)
