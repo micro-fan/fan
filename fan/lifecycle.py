5b4acd592683f12d3a213a8e5ac2d9e597e86d4c
Kirill Pinchuk <cybergrind@gmail.com>
2016-09-15 15:54:56 +0300

    Lifecycle: initial implementation

---
+++ fan/lifecycle.py
@@ -0,0 +1,206 @@
+import time
+import weakref
+import logging
+from enum import IntEnum
+from collections import OrderedDict
+
+
+class LifecycleReason(Exception):
+    pass
+
+
+class SupervisorStop(LifecycleReason):
+    pass
+
+
+class Terminated(LifecycleReason):
+    pass
+
+
+class MaxRestarts(LifecycleReason):
+    pass
+
+
+class RestartType(IntEnum):
+    one_for_one = 1
+    rest_for_one = 2
+    one_for_all = 3
+
+
+class State(IntEnum):
+    INITIALIZED = 0
+    STARTING = 1
+    RUNNING = 2
+    STOPPING = 3
+    STOPPED = 4
+    TERMINATING = 5
+    TERMINATED = 6
+
+
+class RestartStrategy:
+    def __init__(self, restart_type=RestartType.one_for_one, max_r=10, max_t=1):
+        self.restart_type = RestartType
+        self.max_r = max_r
+        self.max_t = max_t
+
+
+def terminate_on_error(fun):
+    def wrapped(self, *args, **kwargs):
+        try:
+            res = fun(self, *args, **kwargs)
+            return res
+        except Exception as e:
+            self.log.debug('Catch exception: {!r}'.format(e))
+            try:
+                if hasattr(self, 'lifecycle'):
+                    self.lifecycle.terminate(e)
+                else:
+                    self.terminate(e)
+            except Exception as term_e:
+                self.log.exception('during terminate')
+            raise e
+    return wrapped
+
+
+class Spec:
+    def __init__(self, cls, *args, strategy=RestartStrategy(), **kwargs):
+        self.cls = cls
+        self.args = args
+        self.strategy = strategy
+        self.kwargs = kwargs
+        self.last_create = 0
+        self.restarts = 0
+
+    def get_obj(self):
+        curr = time.time()
+        if curr - self.last_create < self.strategy.max_t:
+            self.restarts += 1
+            if self.restarts >= self.strategy.max_r:
+                raise MaxRestarts
+        else:
+            self.restarts = 0
+        self.last_create = curr
+        return self.cls(*self.args, *self.kwargs)
+
+    def run(self, obj):
+        obj.lifecycle.on_start()
+        return obj
+
+
+
+
+class Lifecycle:
+    '''
+    We assume that you will set self.lifecycle = Lifecycle()
+    '''
+    log = logging.getLogger('Lifecycle')
+
+    def __init__(self, obj, start_method='', terminate_method='', notify_method=''):
+        self.obj = obj
+        self.start_method = start_method
+        self.terminate_method = terminate_method
+        self.notify_method = notify_method
+        self.links = weakref.WeakSet()
+        self.watches = weakref.WeakSet()
+        self.state = State.INITIALIZED
+
+    @terminate_on_error
+    def on_start(self):
+        assert self.state == State.INITIALIZED, self.state
+        self.state = State.STARTING
+        if self.start_method:
+            getattr(self.obj, self.start_method)()
+        self.state = State.RUNNING
+
+    def safe_call(self, fun, *args, **kwargs):
+        try:
+            return fun(*args, **kwargs)
+        except Exception as e:
+            self.log.exception('during call {} [{} {}]'.format(fun, args, kwargs))
+
+    def terminate(self, reason=Terminated):
+        if self.state >= State.STOPPING:
+            return
+        self.state = State.STOPPING
+        self.log.info('Terminate reason: {!r} {}'.format(reason, self.obj))
+        for link in self.links:
+            self.safe_call(link.terminate, reason, self.obj)
+        for watch in self.watches:
+            self.safe_call(watch.notify, reason, self.obj)
+        self.state = State.STOPPED
+        if self.terminate_method:
+            try:
+                self.state = State.TERMINATING
+                self.safe_call(getattr(self.obj, self.terminate_method), reason)
+            except Exception as e:
+                self.log.exception('during terminate: {}.{}'.format(self.obj,
+                                                                    self.terminate_method))
+        self.state = State.TERMINATED
+
+    @terminate_on_error
+    def notify(self, reason, obj):
+        getattr(self.obj, self.notify_method)(reason, obj)
+
+    def link(self, obj):
+        assert self.state <= State.RUNNING
+        obj.lifecycle.links.add(self)
+
+    def watch(self, obj):
+        assert self.state <= State.RUNNING
+        obj.lifecycle.watches.add(self)
+
+
+class Supervisor:
+    log = logging.getLogger('Supervisor')
+
+    def __init__(self, specs=[]):
+        self.specs = specs
+        self.instances = OrderedDict()
+        self.lifecycle = Lifecycle(self, 'on_start', 'terminate', 'notify')
+
+    @terminate_on_error
+    def notify(self, reason, obj):
+        if self.lifecycle.state > State.RUNNING:
+            return
+        try:
+            spec = self.instances.pop(obj)
+        except KeyError as e:
+            self.log.exception('Get notify without from obj not in self.instances')
+            raise e
+        self.start_child(spec)
+
+    def add_child(self, spec):
+        assert self.lifecycle.state == State.RUNNING, \
+          'Supervisor should be started before add child {}'.format(spec)
+        self.specs.append(spec)
+        self.start_child(spec)
+
+    @terminate_on_error
+    def start_child(self, spec):
+        while True:
+            try:
+                child = spec.get_obj()
+                spec.run(child)
+                self.lifecycle.watch(child)
+                self.instances[child] = spec
+                return child
+            except MaxRestarts as e:
+                raise e
+            except Exception:
+                self.log.info('Restart worker: {}'.format(spec))
+
+    def on_start(self):
+        self.log.debug('Start supervisor')
+        assert self.lifecycle.state == State.STARTING, "don't call on_start directly"
+        for spec in self.specs:
+            self.start_child(spec)
+
+    def terminate(self, reason):
+        assert self.lifecycle.state == State.TERMINATING, "don't call terminate directly"
+        self.log.info('Supervisor terminate reason: {}'.format(reason))
+        while True:
+            try:
+                obj, spec = self.instances.popitem()
+                obj.lifecycle.terminate(reason)
+            except KeyError:
+                break
---
+++ fan/tests/test_lifecycle.py
@@ -0,0 +1,103 @@
+from unittest import TestCase
+from fan.lifecycle import Supervisor, Lifecycle, Spec, MaxRestarts, State
+
+
+class Worker:
+    def __init__(self):
+        self.lifecycle = Lifecycle(self, 'on_start', 'terminate')
+        self.started = False
+        self.terminated = False
+
+    def on_start(self):
+        self.started = True
+
+    def on_stop(self):
+        self.lifecycle.terminate()
+
+    def terminate(self, reason):
+        self.started = False
+        self.terminated = True
+
+
+class DamagedWorker(Worker):
+    def new_on_start(self):
+        self.started = True
+
+    def on_start(self):
+        DamagedWorker.on_start = DamagedWorker.new_on_start
+        raise Exception('damaged worker')
+
+
+class BrokenWorkerError(Exception):
+    pass
+
+
+class BrokenWorker(Worker):
+    def on_start(self):
+        raise BrokenWorkerError
+
+
+class TestSupervisor(Supervisor):
+
+    def __init__(self):
+        self.specs = [Spec(Worker)]
+        super().__init__(self.specs)
+
+    @property
+    def state(self):
+        return self.lifecycle.state
+
+
+class LifecycleTest(TestCase):
+
+    @property
+    def s(self):
+        if hasattr(self, '_s'):
+            return self._s
+        self._s = TestSupervisor()
+        self._s.lifecycle.on_start()
+        return self._s
+
+    def test_01_simple_start(self):
+        self.assertEqual(self.s.state, State.RUNNING)
+        self.assertEqual(len(self.s.instances), 1)
+
+    def test_02_simple_add(self):
+        worker = list(self.s.instances)[0]
+        self.assertEqual(worker.lifecycle.state, State.RUNNING)
+
+    def test_03_worker_restart(self):
+        worker = list(self.s.instances)[0]
+        self.assertEqual(worker.lifecycle.state, State.RUNNING)
+        worker.lifecycle.terminate()
+        self.assertEqual(worker.lifecycle.state, State.TERMINATED)
+
+        worker = list(self.s.instances)[0]
+        self.assertEqual(worker.lifecycle.state, State.RUNNING)
+
+    def test_04_damaged_restart(self):
+        self.s.add_child(Spec(DamagedWorker))
+        workers = list(self.s.instances)
+        self.assertEqual(len(workers), 2)
+        worker2 = workers[1]
+        self.assertEqual(self.s.state, State.RUNNING)
+        self.assertEqual(worker2.lifecycle.state, State.RUNNING)
+
+        self.s.lifecycle.terminate()
+        self.assertEqual(self.s.lifecycle.state, State.TERMINATED)
+        self.assertEqual(len(self.s.instances), 0)
+        self.assertEqual(workers[0].lifecycle.state, State.TERMINATED)
+        self.assertEqual(workers[1].lifecycle.state, State.TERMINATED)
+
+    def test_05_add_broken_worker(self):
+        s = TestSupervisor()
+        s.lifecycle.on_start()
+
+        self.assertRaises(MaxRestarts, s.add_child, Spec(BrokenWorker))
+        self.assertEqual(s.lifecycle.state, State.TERMINATED)
+
+    def test_06_broken_supervisor(self):
+        s = Supervisor([Spec(BrokenWorker)])
+        self.assertRaises(MaxRestarts, s.lifecycle.on_start)
+        self.assertEqual(list(s.instances), [])
+        self.assertEqual(s.lifecycle.state, State.TERMINATED)
