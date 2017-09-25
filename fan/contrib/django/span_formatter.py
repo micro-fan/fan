2df225cb0f2e3b8f6f32365a40a9b835537929f4
Kirill Pinchuk <cybergrind@gmail.com>
2017-09-25 18:53:10 +0300

    Add SpanFormatter

---
+++ fan/contrib/django/span_formatter.py
@@ -0,0 +1,8 @@
+from fan.contrib.django import VARS
+from tipsi_tools.logging import JSFormatter
+
+
+class SpanFormatter(JSFormatter):
+    def process_log_record(self, rec):
+        rec.update(VARS)
+        return super().process_log_record(rec)
